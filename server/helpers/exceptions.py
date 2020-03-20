0e4ae74d5d75fb3a5f47bc20e12e9574545508fe
amoniaka-knabino <sv@dynfor.ru>
2020-03-20 13:22:18 +0500

    don't send addition if error occured

--- server/helpers/exceptions.py
+++ server/helpers/exceptions.py
@@ -31,32 +31,26 @@ def handle_exception(exception, addition):
     if type(exception) is UserNotFoundException:
         http_code = 404
         return jsonify({"status": http_code, "result": False,
-                        "addition": addition,
                         "description": "User is not found in database"}),http_code
     if type(exception) is InvalidSumException:
         http_code = 400
         return jsonify({"status": http_code, "result": False,
-                        "addition": addition,
                         "description": "Sum param can't be negative"}), http_code
     if type(exception) is SomeOtherArgsRequired:
         http_code = 400
         return jsonify({"status": http_code, "result": False,
-                        "addition": addition,
                         "description": "Not all reqired params were"}), http_code
     if type(exception) is ClosedAccountException:
         http_code = 403
         return jsonify({"status": http_code, "result": False,
-                        "addition": addition,
                         "description": "Can't execue operations with closed account"}), http_code
     if type(exception) is JSONObjectExpected:
         http_code = 400
         return jsonify({"status": http_code, "result": False,
-                        "addition": addition,
                         "description": "Bad JSON"}), http_code
     if type(exception) is psycopg2.errors.UniqueViolation:
         http_code = 400
         return jsonify({"status": http_code, "result": False,
-                        "addition": addition,
                         "description": "Tried to add subscriber who are already exists in database"}), http_code
     else:
         print(exception)
